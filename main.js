3f247e79ac752be10215cc20831e7a10f12e7589
Anselem <anselemodims@gmail.com>
2021-11-09 11:17:33 +0100

    Modularized the functions

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-import { clickHandler, hamburger } from './utils.js';
+import {
+  clickHandler,
+  hamburger,
+  navList,
+  activeHandler,
+} from './utils.js';
 
+// hamburger toggle
 hamburger.addEventListener('click', clickHandler);
+
+// nav list active toggle
+navList.forEach((item) => {
+  item.addEventListener('click', activeHandler);
+});
--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 import helperFunc from './helpers.js';
 
 export const hamburger = document.querySelector('.toggle');
-const navList = document.querySelectorAll('.nav-list');
+export const navList = document.querySelectorAll('.nav-list');
 export const dropDown = document.querySelector('lang-dropdown');
 const navMenu = document.querySelector('header > nav > .hamburger');
 
@@ -41,9 +41,7 @@ export const clickHandler = (e) => {
 };
 
 // nav list active toggle
-navList.forEach((item) => {
-  item.addEventListener('click', (e) => {
-    document.querySelector('.nav-list.active').classList.remove('active');
-    e.currentTarget.classList.add('active');
-  });
-});
+export const activeHandler = (e) => {
+  document.querySelector('.nav-list.active').classList.remove('active');
+  e.currentTarget.classList.add('active');
+};
