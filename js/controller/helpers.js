88cb6e38f80b718f2bb2f53ce5cb2ed62fce582f
Anselem <anselemodims@gmail.com>
2021-11-10 05:19:30 +0100

    Included the form validation handlers and techniques

--- js/controller/helpers.js
+++ js/controller/helpers.js
@@ -1,5 +1,8 @@
 const main = document.querySelector('main');
 const hide = document.querySelectorAll('.hide');
+export const inputs = document.querySelectorAll('input');
+export const form = document.querySelector('.contact-form form');
+const small = document.querySelector('small');
 
 /**
  *@function {helperFunc} helperFunc
@@ -16,20 +19,81 @@ export const helperFunc = (blur, list, display, left) => {
   list.style.left = left;
 };
 
+/**
+ * @function findItem Handles finding an object from an array of objects
+ * @param {HTMLElement} target The HTML element to be targetted
+ * @param {Array} model An array of objects
+ * @returns {Object} The object found in the array
+ */
 export const findItem = (target, model) => {
   const { btnId } = target.dataset;
   return model.find((elem) => elem.id === parseInt(btnId, 10));
 };
 
+/**
+ *
+ * @param {string} message The error message to be shown
+ */
 export const showErr = (message) => {
   document.querySelector('small').innerText = message;
 };
 
+/**
+ *
+ * @param {HTMLSmallElement} small The HTML small element that outputs the error
+ */
 export const showSuccess = (small) => {
   small.innerHTML = '';
 };
 
+/**
+ *
+ * @param {classList} classes The classlist of an element
+ * @param {string} error The error string
+ * @param {string} success The success string
+ */
 export const show = (classes, error, success) => {
   classes.remove(success);
   classes.add(error);
+};
+
+// Regex to check if the input include a uppercase case letter
+const re = /[A-Z]/g;
+
+/**
+ * @function formHandler Handles the validation on the form submit event
+ * @param {event} e The event object
+ */
+export const formHandler = (e) => {
+  const [name, email] = [inputs[0].value.trim(), inputs[1].value.trim()];
+  if (name === '' || name.length < 5) {
+    e.preventDefault();
+    showErr('Name should be at least 5 characters long');
+  } else if (email === '' || /[A-Z]/.test(email)) {
+    e.preventDefault();
+    showErr('Email should contain only lowercase characters');
+    inputs[1].value = email.toLowerCase();
+  } else {
+    showSuccess(small);
+    form.submit();
+  }
+};
+
+/**
+ * @function blurHandler Handles the blur event
+ * @param {event} e The event object
+ */
+export const blurHandler = (e) => {
+  const [target, value, classes] = [
+    e.currentTarget.type,
+    e.currentTarget.value,
+    e.currentTarget.classList,
+  ];
+  if ((target === 'text' && value.length < 5) || value === '') {
+    show(classes, 'error', 'success');
+  } else if ((target === 'email' && re.test(value)) || value === '') {
+    show(classes, 'error', 'success');
+  } else {
+    show(classes, 'success', 'error');
+  }
 };
